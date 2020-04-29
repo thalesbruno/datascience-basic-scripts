9eba64b9b1df1ede67641e7dbd6a3a72056e0664
Thales Bruno <thalesbrunom@gmail.com>
2020-04-29 10:21:35 -0300

    create first scripts

---
+++ egrep.py
@@ -0,0 +1,16 @@
+# egrep.py
+# 
+# This script reads text from stdin (line by line)
+# and spits back out the lines that match a regular expression
+
+import re, sys
+
+# sys.argv is the list of command-line arguments
+# sys.argv[0] is the name of the program itself
+# sys.argv[1] will be the regex specified at the command line
+regex = sys.argv[1]
+
+# for every line passed into script
+for line in sys.stdin:
+    if re.search(regex, line):
+        sys.stdout.write(line)
