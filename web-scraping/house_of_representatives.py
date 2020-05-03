5d20f089ba9d89dca0baccb6f4d0e4d13a5f1fd1
Thales Bruno <thalesbrunom@gmail.com>
2020-05-03 18:12:27 -0300

    finish house of representatives web scrap script

--- web-scraping/house_of_representatives.py
+++ web-scraping/house_of_representatives.py
@@ -23,6 +23,8 @@ print(f"{len(all_urls)} urls")
 print(f"{len(good_urls)} good urls")
 print(f"{len(unique_good_urls)} unique good urls")
 
+print(f"searching in all congresspeople press releases for 'data' mentions...")
+
 # getting press releases links from each house representative page
 press_releases: Dict[str, Set[str]] = {}
 
@@ -42,13 +44,24 @@ for house_url in unique_good_urls:
 
     # discarding empty sets
     if pr_links:
-        print(f"{house_url}: {pr_links}")
+        # print(f"{house_url}: {pr_links}")
         press_releases[house_url] = pr_links
 
-print(f"press releases:")
 
-for house_url, pr_links in press_releases:
-    print(f"{house_url}: {pr_links}")
+def paragraph_mentions(text: str, keyword: str) -> bool:
+    """
+    Returns True if a <p> inside the text mentions {keyword}
+    """
+    soup = BeautifulSoup(text, 'html5lib')
+    paragraphs = [p.get_text() for p in soup('p')]
 
-# some work to do
-# handling double slash between absolute and relative parts of links
+    return any(keyword.lower() in paragraph.lower() for paragraph in paragraphs)
+
+
+for house_url, pr_links in press_releases.items():
+    for pr_link in pr_links:
+        url = f"{pr_link}"
+        text = requests.get(url).text
+
+        if paragraph_mentions(text, 'data'):
+            print(f"{house_url} has mentioned 'data")
