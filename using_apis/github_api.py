8f7e3664c7035c503c718823e74607422d4b883f
Thales Bruno <thalesbrunom@gmail.com>
2020-05-04 07:25:09 -0300

    add using_apis folder and github script

---
+++ using_apis/github_api.py
@@ -0,0 +1,20 @@
+# github_api.py
+import json
+import requests
+from collections import Counter
+from dateutil.parser import parse
+
+github_user = "joelgrus"
+endpoint = f"https://api.github.com/users/{github_user}/repos"
+
+repos = json.loads(requests.get(endpoint).text)
+
+# print(type(repos))
+print(f"{github_user} has {len(repos)} public repos")
+
+dates = [parse(repo['created_at']) for repo in repos]
+month_counts = Counter(date.month for date in dates)
+weekday_counts = Counter(date.weekday() for date in dates)
+
+print(month_counts)
+print(weekday_counts)
