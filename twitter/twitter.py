c4d27d6ad562a971bc1b1578a397f71fc09520d5
Thales Bruno <thalesbrunom@gmail.com>
2020-05-06 08:43:22 -0300

    create twython instance and first statement

--- twitter/twitter.py
+++ twitter/twitter.py
@@ -8,4 +8,18 @@ with open('credentials.json', 'r') as f:
 CONSUMER_KEY = credentials['consumer_key']
 CONSUMER_SECRET = credentials['consumer_secret']
 ACCESS_TOKEN = credentials['access_token']
-ACCESS_TOKEN_SECRET = credentials['access_token_access']
+ACCESS_TOKEN_SECRET = credentials['access_token_secret']
+
+twitter = Twython(CONSUMER_KEY,
+                  CONSUMER_SECRET,
+                  ACCESS_TOKEN,
+                  ACCESS_TOKEN_SECRET)
+
+# Checking credentials
+# twitter.verify_credentials()
+
+# Search for tweet containing the phrase "data science"
+for status in twitter.search(q='"brumado"')['statuses']:
+    user = status['user']['screen_name']
+    text = status['text']
+    print(f"{user}: {text}\n")
